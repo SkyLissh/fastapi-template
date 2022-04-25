11d5084e22c2efac66a3d13796fe58554d72cdd7
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-04-25 18:43:27 -0500

    :sparkles: Crud Base
    Implementing a crud base class

---
+++ app/crud/__init__.py
@@ -0,0 +1,2 @@
+# Exports all crud classes
+from app.crud.crud_base import CrudBase
---
+++ app/crud/crud_base.py
@@ -0,0 +1,106 @@
+from typing import Any, Generic, Optional, Type, TypeVar, Union
+from uuid import UUID
+
+from app.core.db.base_class import Base
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy import delete, insert, select, update
+from sqlalchemy.orm import Session
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class CrudBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]) -> None:
+        """
+        CRUD object with default methods to Create, Read, Update, Delete (CRUD).
+        **Parameters**
+        * `model`: A SQLAlchemy model class
+        * `schema`: A Pydantic model (schema) class
+        """
+        self.model = model
+
+    def get(self, db: Session, id: UUID) -> Optional[ModelType]:
+        """
+        Get a single record by id.
+        **Parameters**
+        * `id`: Record id
+        * `db`: SQLAlchemy session
+        **Returns**
+        * A single record
+        #"""
+        select_query = select([self.model]).where(self.model.id == id)
+        return db.execute(select_query).scalar()
+
+    def get_all(
+        self,
+        db: Session,
+        *,
+        skip: int = 0,
+        limit: int = 100,
+    ) -> list[ModelType]:
+        """
+        Get all records.
+        **Parameters**
+        * `db`: SQLAlchemy session
+        **Returns**
+        * A list of records
+        """
+        select_query = select([self.model]).offset(skip).limit(limit)
+        return db.execute(select_query).scalars().all()
+
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        """
+        Create a new record.
+        **Parameters**
+        * `db`: SQLAlchemy session
+        * `obj_in`: Pydantic model (schema)
+        **Returns**
+        * A new record
+        """
+        obj_in_data = jsonable_encoder(obj_in)
+        result = db.execute(insert(self.model).values(**obj_in_data))
+        db.commit()
+
+        return self.get(db, result.inserted_primary_key[0])  # type: ignore
+
+    def update(
+        self,
+        db: Session,
+        id: UUID,
+        *,
+        obj_in: Union[UpdateSchemaType, dict[str, Any]],
+    ) -> Optional[ModelType]:
+        """
+        Update a record.
+        **Parameters**
+        * `db`: SQLAlchemy session
+        * `id`: Record id
+        * `obj_in`: Pydantic model (schema)
+        **Returns**
+        * `returns` A new record
+        """
+        update_data = (
+            obj_in if isinstance(obj_in, dict) else obj_in.dict(exclude_unset=True)
+        )
+
+        update_query = (
+            update(self.model).where(self.model.id == id).values(**update_data)
+        )
+        db.execute(update_query)
+        db.commit()
+
+        return self.get(db, id)
+
+    def delete(self, db: Session, *, id: UUID) -> None:
+        """
+        Delete a record.
+        **Parameters**
+        * `db`: SQLAlchemy session
+        * `id`: Record id
+        """
+        delete_query = delete(self.model).where(self.model.id == id)
+        db.execute(delete_query)
+        db.commit()
