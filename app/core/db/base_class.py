bdfdb618252a86f40cd4cec193c9dde88c82afc2
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-04-25 16:25:48 -0500

    :bricks: Base Model Class
    Create a base class from which the models inherit

---
+++ app/core/db/base_class.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+from sqlalchemy_utils import UUIDType
+
+
+@as_declarative()
+class Base:
+    id: UUID = Column(UUIDType(), primary_key=True, default=uuid4())
+    created_at: datetime = Column(DateTime, default=func.current_timestamp())
+    update_at: datetime = Column(
+        DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp()
+    )
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + "s"
