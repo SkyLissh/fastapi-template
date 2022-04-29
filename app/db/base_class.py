b5f068e1eb0a1d42480b14e74c1a0a30aa1c3dc2
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-04-29 14:41:39 -0500

    :art: Change created_at and updated_at
    Change those fields fr manage date

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import as_declarative
+from sqlalchemy.sql import func
 from sqlalchemy_utils import UUIDType
 
 
 @as_declarative()
 class Base:
     id: UUID = Column(UUIDType(), primary_key=True, default=uuid4())
-    created_at: datetime = Column(DateTime, default=func.current_timestamp())
+    created_at: datetime = Column(DateTime, server_default=func.now())
     update_at: datetime = Column(
-        DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp()
+        DateTime, server_default=func.now(), onupdate=func.now()
     )
 
     __name__: str
